50a6f4f304dcb5b7d3a1123b59048c22782fb70b
Zach <147861361+1054009@users.noreply.github.com>
2024-04-15 13:36:56 -0400

    Rename index to home

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ conn = engine.connect()
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("home.html")
 
 @app.route("/boats/")
 @app.route("/boats/<page>")
