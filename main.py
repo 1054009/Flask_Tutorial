2737c576bcb0ad9fce83c811db4877d7750cf1cf
Zach <147861361+1054009@users.noreply.github.com>
2024-04-15 12:58:12 -0400

    Fix indentation

--- main.py
+++ main.py
@@ -7,36 +7,36 @@ conn = engine.connect()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route("/boats/")
 @app.route("/boats/<page>")
 def get_boats(page = 1):
-    page = int(page)
-    per_page = 10
+	page = int(page)
+	per_page = 10
 
 	query = f"SELECT * FROM boats LIMIT {per_page} OFFSET {(page - 1) * per_page}"
 
-    boats = conn.execute(query).all()
+	boats = conn.execute(query).all()
 
-    return render_template("boats.html", boats=boats, page=page, per_page=per_page)
+	return render_template("boats.html", boats=boats, page=page, per_page=per_page)
 
 @app.route("/create", methods=["GET"])
 def create_get_request():
-    return render_template("boats_create.html")
+	return render_template("boats_create.html")
 
 @app.route("/create", methods=["POST"])
 def create_boat():
-    try:
-        conn.execute(
-            text("INSERT INTO boats values (:id, :name, :type, :owner_id, :rental_price)"),
-            request.form
-        )
-        return render_template("boats_create.html", error=None, success="Data inserted successfully!")
-    except Exception as e:
-        error = e.orig.args[1]
-        print(error)
-        return render_template("boats_create.html", error=error, success=None)
+	try:
+		conn.execute(
+			text("INSERT INTO boats values (:id, :name, :type, :owner_id, :rental_price)"),
+			request.form
+		)
+		return render_template("boats_create.html", error=None, success="Data inserted successfully!")
+	except Exception as e:
+		error = e.orig.args[1]
+		print(error)
+		return render_template("boats_create.html", error=error, success=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
